f9681dba4f0c5c5fd1537ce2eb9195877f2eb7c7
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-03-20 17:13:29 +0000

    Implement site structure with header, main, and menu components; add global styles and Tailwind CSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     '/sngrcreative': 'https://sngrcreative.pages.dev'
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 })
